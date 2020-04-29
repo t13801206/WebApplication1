bcc65a518d23dee49adfe774b21735d81acaa163
t13801206 <t13801206@yahoo.co.jp>
2020-04-29 14:05:34 +0900

    プロジェクト ファイルを追加します。

---
+++ WebApplication1/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string imgPath { get; set; } = @"C:/Users/naoto/Documents/FTPDir/1.png";
+    }
+}
